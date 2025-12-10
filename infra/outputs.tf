537a6b2b833196e05cb8a2bce4c0d0eef9813286
Danilburov <499732@student.fontys.nl>
2025-12-10 18:22:33 +0100

    Fix: Temporary commenting out the outputs.tf file

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,22 +1,15 @@
-output "eks_cluster_name" {
-  value = module.eks.cluster_name
-}
+# output "eks_cluster_name" {
+#   value = module.eks.cluster_name
+# }
 
-output "eks_cluster_endpoint" {
-  value = module.eks.cluster_endpoint
-}
+# output "eks_cluster_endpoint" {
+#   value = module.eks.cluster_endpoint
+# }
 
-output "hr_db_endpoint" {
-  value = aws_db_instance.hr.address
-}
+# output "hr_db_endpoint" {
+#   value = aws_db_instance.hr.address
+# }
 
-output "hr_db_port" {
-  value = aws_db_instance.hr.port
-}
-output "hr_db_endpoint" {
-  value = aws_db_instance.hr.address
-}
-
-output "hr_db_port" {
-  value = aws_db_instance.hr.port
-}
+# output "hr_db_port" {
+#   value = aws_db_instance.hr.port
+# }
